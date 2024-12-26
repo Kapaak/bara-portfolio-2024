ae84cdecee9ffc1d6c453c76d24fd798095754f8
Pavel Zapletal <pavel.zaplet25@gmail.com>
2024-12-26 14:29:13 +0100

    feat: add vercel config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  adapter: vercel({
+    edgeMiddleware: true,
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
