3f0e3d0fdf0e68fc97c2c6d19e12c59bedc87c3b
Pavel Zapletal <pavel.zaplet25@gmail.com>
2024-12-26 14:54:38 +0100

    feat: get projects from sanity

---
+++ src/sanity/api.ts
@@ -0,0 +1,19 @@
+import groq from "groq";
+import { sanityClient } from "sanity:client";
+
+type SanityProject = any; //TODO: generate from SANITY schema!
+
+export async function getProjects(): Promise<SanityProject[]> {
+  const query = groq`*[_type == "project"]{"id":_id,name,description,"slug":slug.current,url,
+   tags[]-> {
+    _id,
+    name,
+    "value":value.current
+  },
+  image,
+  }`;
+
+  const projects: SanityProject[] = await sanityClient.fetch(query);
+
+  return projects;
+}
