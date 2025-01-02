05667db0f50125fb36c1e2b1d212cb2367b0bce5
Pavel Zapletal <pavel.zaplet25@gmail.com>
2025-01-02 16:36:19 +0100

    feat: project detail hero section

--- src/sanity/api.ts
+++ src/sanity/api.ts
@@ -2,6 +2,7 @@ import groq from "groq";
 import { sanityClient } from "sanity:client";
 
 type SanityProject = any; //TODO: generate from SANITY schema!
+type SanityProjectDetail = any; //TODO: generate from SANITY schema!
 
 export async function getProjects(): Promise<SanityProject[]> {
   const query = groq`*[_type == "project"]{"id":_id,name,description,"slug":slug.current,url,
@@ -14,6 +15,31 @@ export async function getProjects(): Promise<SanityProject[]> {
   "imageAlt":image.alt,
   orderRank,
   }|order(orderRank)`;
+
+  const projects: SanityProject[] = await sanityClient.fetch(query);
+
+  return projects;
+}
+
+export async function getProjectDetailBySlug(
+  projectSlug?: string
+): Promise<SanityProjectDetail> {
+  const query = groq`*[_type == "projectDetail" && parentProject->slug.current == "${projectSlug}"]{"id":_id,
+   parentProject-> {
+    _id,
+    name,description,"slug":slug.current,url,
+    year,
+    image{asset->{...,metadata}},
+    "imageAlt":image.alt,
+    tags[]-> {
+    _id,
+    name,
+    "value":value.current
+  },
+  },
+   usedTechnologies,
+   gallery[]{asset->{...,metadata}},
+  }[0]`;
 
   const projects: SanityProject[] = await sanityClient.fetch(query);
 
