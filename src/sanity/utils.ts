bc8d1fb6485bc2d0a00f5d81747462e6f94a9de5
Pavel Zapletal <pavel.zaplet25@gmail.com>
2025-01-10 17:59:36 +0100

    fix: mobile detection

--- src/sanity/utils.ts
+++ src/sanity/utils.ts
@@ -1,17 +1,9 @@
-import imageUrlBuilder from "@sanity/image-url";
-import type { SanityImageSource } from "@sanity/image-url/lib/types/types";
-import { sanityClient } from "sanity:client";
-
-import type { SanityFile } from "./types";
+import imageUrlBuilder from '@sanity/image-url';
+import type { SanityImageSource } from '@sanity/image-url/lib/types/types';
+import { sanityClient } from 'sanity:client';
 
 const builder = imageUrlBuilder(sanityClient);
 
 export function urlFor(source: SanityImageSource) {
   return builder?.image(source);
-}
-
-export function getFileUrl(source: SanityFile) {
-  if (!source) return null;
-
-  return source?.asset?.url;
 }
